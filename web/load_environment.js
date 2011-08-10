7604a6110968525c6e93ccbd8905d755a0673002
Guilherme J. Tramontina <guilherme.tramontina@gmail.com>
2011-08-09 22:26:10 -0300

    Let it throw the exception...

--- web/load_environment.js
+++ web/load_environment.js
@@ -1,11 +1,10 @@
 var fileSystem = require('fs');
 var environmentFile = '/home/dotcloud/environment.json';
-try {
-  var environment = JSON.parse(fileSystem.readFileSync(environmentFile));
-  environment.forEach(function(variable) {
-    console.log('Setting '+variable+'...');
-    process.env[variable] = environment[variable];
-  });
-} catch(e) { /* o_O */ }
+
+var environment = JSON.parse(fileSystem.readFileSync(environmentFile));
+environment.forEach(function(variable) {
+  console.log('Setting '+variable+'...');
+  process.env[variable] = environment[variable];
+});
 
 require(__dirname+'/'+process.argv.slice(2)[0]);
